0b849c34e02513d187a97a864e87cb27c7182245
Aaron Bedra <aaron@aaronbedra.com>
2009-12-07 17:21:57 -0500

    updating ruby installer

--- osx/.zsh/ruby_installer.zsh
+++ osx/.zsh/ruby_installer.zsh
@@ -22,25 +22,25 @@ function install_ruby_187 {
 
 function install_ruby_191 {
   mkdir -p ~/.ruby_versions && cd /tmp
-  curl -L -O ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.1-p243.tar.gz &&
-  tar xzf ruby-1.9.1-p243.tar.gz &&
-  cd ruby-1.9.1-p243 &&
+  curl -L -O ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.1-p376.tar.gz &&
+  tar xzf ruby-1.9.1-p376.tar.gz &&
+  cd ruby-1.9.1-p376 &&
   ./configure --prefix=$HOME/.ruby_versions/ruby_191 --enable-shared &&
   make && make install &&
-  rm -rf ruby-1.9.1-p243.tar.gz ruby-1.9.1-p243 &&
+  rm -rf ruby-1.9.1-p376.tar.gz ruby-1.9.1-p376 &&
   use_ruby_191 && install_rubygems && install_basic_gems && cd ~
 }
 
-function install_jruby_131 {
+function install_jruby_140 {
   mkdir -p ~/.ruby_versions && cd ~/.ruby_versions &&
-  curl -O -L --silent http://dist.codehaus.org/jruby/1.3.1/jruby-bin-1.3.1.zip &&
-  rm -rf jruby-1.3.1 &&
-  jar xf jruby-bin-1.3.1.zip &&
-  ln -sf ~/.ruby_versions/jruby-1.3.1/bin/jruby ~/.ruby_versions/jruby-1.3.1/bin/ruby &&
-  ln -sf ~/.ruby_versions/jruby-1.3.1/bin/jgem ~/.ruby_versions/jruby-1.3.1/bin/gem &&
-  ln -sf ~/.ruby_versions/jruby-1.3.1/bin/jirb ~/.ruby_versions/jruby-1.3.1/bin/irb &&
-  chmod +x ~/.ruby_versions/jruby-1.3.1/bin/{jruby,jgem,jirb} &&
-  rm -rf ~/.ruby_versions/jruby-bin-1.3.1.zip &&
+  curl -O -L --silent http://jruby.kenai.com/downloads/1.4.0/jruby-bin-1.4.0.zip &&
+  rm -rf jruby-1.4.0 &&
+  jar xf jruby-bin-1.4.0.zip &&
+  ln -sf ~/.ruby_versions/jruby-1.4.0/bin/jruby ~/.ruby_versions/jruby-1.4.0/bin/ruby &&
+  ln -sf ~/.ruby_versions/jruby-1.4.0/bin/jgem ~/.ruby_versions/jruby-1.4.0/bin/gem &&
+  ln -sf ~/.ruby_versions/jruby-1.4.0/bin/jirb ~/.ruby_versions/jruby-1.4.0/bin/irb &&
+  chmod +x ~/.ruby_versions/jruby-1.4.0/bin/{jruby,jgem,jirb} &&
+  rm -rf ~/.ruby_versions/jruby-bin-1.4.0.zip &&
   use_jruby_131 && install_jruby_openssl && install_basic_gems &&
   cd ~
 }
@@ -48,7 +48,7 @@ function install_jruby_131 {
 function make_rubies_go_now {
   install_ruby_186
   install_ruby_191
-  install_jruby_131
+  install_jruby_140
 }
 
 function install_rubygems {
