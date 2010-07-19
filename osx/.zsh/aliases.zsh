2782febcfc182ad8deeff98567d5becd887f14ea
Aaron Bedra <aaron@aaronbedra.com>
2010-07-19 12:56:06 -0400

    add new rails based functions over aliases

--- osx/.zsh/aliases.zsh
+++ osx/.zsh/aliases.zsh
@@ -1,16 +1,15 @@
-alias g='git'
 alias port='sudo port -v'
 alias ssh='/usr/bin/ssh'
+
 alias ls='ls -GFh'
 alias ll='ls -GFhl'
-alias rmate='mate Rakefile README app bin config doc db lib public script spec test stories features examples'
+
 alias ri='ri -Tf ansi'
 alias rdoc='rdoc --ri --line-numbers --all'
-alias ss='./script/server'
-alias sc='./script/console'
+
 alias vi='vim'
 alias vim='vim'
 alias gvim='mvim -p'
 alias e='open -a Emacs'
-alias m='mate'
+
 alias cycle_passenger='touch tmp/restart.txt'
